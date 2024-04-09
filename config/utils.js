8793633575eaef5c5e500dcb545596eaaae0070a
jellgrum <jellgrum@gmail.com>
2024-04-09 21:43:07 +0300

    standardize exports

--- config/utils.js
+++ config/utils.js
@@ -3,14 +3,19 @@ import path from 'path';
 import { globSync } from 'glob';
 
 
-export const getArgument = (argumentName) => {
+const getArgument = (argumentName) => {
   const argument = process.argv.find(arg => arg.startsWith('--') && arg.includes(`${argumentName}=`));
   const [, value] = (argument || '').split('=');
   return value || '';
 };
 
-export const getWorkDirFileExtensions = (workDir) => {
+ const getWorkDirFileExtensions = (workDir) => {
   const filePaths = globSync(`${workDir}/**/*`, { withFileTypes: true });
   const extensions = filePaths.map(posix => path.extname(posix.name).slice(1)).filter(Boolean);
   return [...new Set(extensions)];
+};
+
+export {
+  getArgument,
+  getWorkDirFileExtensions,
 };
